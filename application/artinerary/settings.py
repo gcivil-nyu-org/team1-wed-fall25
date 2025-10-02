ec283a4821dfbc6b03255a5f8bd0205b0dc14352
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-10-02 19:56:41 -0400

    Try adding PostgreSQL as database in settings, move credentials to .env

--- application/artinerary/settings.py
+++ application/artinerary/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v7z0ofi*!g*tkv-tuu*chnf=rukn0u@u&%o+8io7q+g(&)ytw_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY_FALLBACKS = [
+        os.environ.get('SECRET_KEY_FALLBACK'),
+    ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +80,12 @@ WSGI_APPLICATION = 'artinerary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',  # Or the IP address/hostname of your PostgreSQL server
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
