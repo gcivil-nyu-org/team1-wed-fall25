f215e1f412681b0baa5133aa1e009c787b5e6c55
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-10-04 19:13:23 -0400

    Try adding database info to settings and django.config

--- application/artinerary/settings.py
+++ application/artinerary/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY_FALLBACKS = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['artinerary-dev.us-east-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'artinerary-dev.us-east-2.elasticbeanstalk.com'
+    ]
 
 
 # Application definition
@@ -78,16 +81,28 @@ WSGI_APPLICATION = 'artinerary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': 'localhost',  # Or the IP address/hostname of your PostgreSQL server
-        'PORT': '5432',       # Default PostgreSQL port
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': 'localhost',  # Or the IP address/hostname of your PostgreSQL server
+            'PORT': '5432',       # Default PostgreSQL port
+        }
+    }
 
 
 # Password validation
