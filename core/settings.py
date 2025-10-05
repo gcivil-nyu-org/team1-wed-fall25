cad0d7ab80180778cf77fef1c78f9b60ac3caa5c
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-10-05 08:53:10 -0400

    Rearrange file structure and rename application folder to core

--- core/settings.py
+++ core/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'artinerary.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -75,7 +75,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'artinerary.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
