90f804f3bd4b390faa689d4b0fb1de11efb2716f
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-10-11 07:50:29 -0400

    Add text change to test deploy

--- loc_detail/views.py
+++ loc_detail/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("This is where location details will be.")
+    return HttpResponse("This is where location details will be. Does this still deploy?")
