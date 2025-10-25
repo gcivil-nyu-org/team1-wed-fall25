e76aa7e030a702d953c7ffdfe8532017d543cb73
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-10-25 14:07:23 -0400

    Update PublicArt model, settings, migrations, requirements to add images in S3

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("accounts/", include("accounts.urls")),
@@ -24,3 +26,6 @@ urlpatterns = [
     path("loc_detail/", include("loc_detail.urls")),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
