889cc6994d271e936a31f3e8e4f10d9aad71f0f0
Zeudon <damodaran.nived@gmail.com>
2025-11-11 16:59:43 -0500

    reverifying email when updating user profile

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
         "remove-profile-image/", views.remove_profile_image, name="remove_profile_image"
     ),
     path("edit/profile/", views.edit_profile, name="edit_profile"),
+    path("verify-email-change/", views.verify_email_change, name="verify_email_change"),
+    path("resend-email-change-otp/", views.resend_email_change_otp, name="resend_email_change_otp"),
     path("", views.profile_view, name="my_profile"),
     path("<str:username>/", views.profile_view, name="profile_view"),
     # Follow functionality
