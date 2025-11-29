b057835b0627914dd006d8c02dbfbee9faf104bb
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2025-11-29 16:35:51 -0500

    Update events and chat messages

--- static/js/map/events.js
+++ static/js/map/events.js
@@ -8,7 +8,7 @@ export class EventMarkerManager {
         this.map = map;
         this.eventMarkers = L.layerGroup();
     }
-    
+
     /**
      * Load event markers from API and add to map
      */
@@ -16,9 +16,9 @@ export class EventMarkerManager {
         try {
             const response = await fetch('/events/api/pins/');
             const data = await response.json();
-            
+
             console.log(`Loaded ${data.points.length} event locations`);
-            
+
             // Create markers for each event
             data.points.forEach(event => {
                 // Create purple marker for events
@@ -30,40 +30,39 @@ export class EventMarkerManager {
                     popupAnchor: [1, -34],
                     shadowSize: [41, 41]
                 });
-                
+
                 const marker = L.marker([event.y, event.x], { icon: purpleIcon });
-                
+
                 // Create popup with event info
                 const popupContent = `
                     <div class="event-popup">
                         <h3 style="margin: 0 0 0.5rem 0;">${event.t}</h3>
                         <p style="margin: 0 0 0.5rem 0;"><strong>Event</strong></p>
                         <a href="/events/${event.slug}/" 
-                           style="display: inline-block; padding: 0.5rem 1rem; background: #007bff; 
-                                  color: white; text-decoration: none; border-radius: 4px;">
+                           class="btn btn-primary">
                             View Event
                         </a>
                     </div>
                 `;
-                
+
                 marker.bindPopup(popupContent);
-                
+
                 // Add click handler to navigate to event detail
                 marker.on('click', () => {
                     // Popup will show with the link
                 });
-                
+
                 this.eventMarkers.addLayer(marker);
             });
-            
+
             // Add event markers layer to map
             this.map.addLayer(this.eventMarkers);
-            
+
         } catch (error) {
             console.error('Error loading event locations:', error);
         }
     }
-    
+
     /**
      * Remove all event markers from map
      */
